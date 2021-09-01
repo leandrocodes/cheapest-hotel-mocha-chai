6666d9f37dbfa588bed884da26c0e40df000e5e4
leandrocodes <leandrovianacodes@gmail.com>
2021-08-31 22:32:40 -0400

    hotels object map

---
+++ src/hotels.js
@@ -0,0 +1,22 @@
+const hotels = [
+	{
+		name: 'Parque das Flores',
+		rating: 3,
+		weekday: { regular: 110, fidelity: 80 },
+		weekend: { regular: 90, fidelity: 80 }
+	},
+	{
+		name: 'Jardim Botânico',
+		rating: 4,
+		weekday: { regular: 160, fidelity: 110 },
+		weekend: { regular: 60, fidelity: 50 }
+	},
+	{
+		name: 'Mar Atlântico',
+		rating: 5,
+		weekday: { regular: 220, fidelity: 100 },
+		weekend: { regular: 150, fidelity: 40 }
+	}
+]
+
+module.exports = hotels
